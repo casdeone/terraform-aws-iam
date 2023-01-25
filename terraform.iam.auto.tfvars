1792b6c8e9dacee230da8d8015e33f5362c439a1
Dennis Castillo <dennis.castillo@sharp.com>
2023-01-25 08:11:54 -0800

    bootstrap_repo

---
+++ terraform.iam.auto.tfvars
@@ -0,0 +1,2 @@
+vpc_id = "vpc-0df036381f83a6c8a"
+permissions_boundary = "arn:aws:iam::938456191170:policy/Test-Permission-Boundary"
--- variables.tf
+++ variables.tf
@@ -1,1 +1,20 @@
 #create variables
+variable "region" {
+    type = string
+    default = "us-west-2"
+  
+}
+variable "role_session_name" {
+    type = string
+    default = "terraform-cloud-session"
+}
+
+variable "assume_role_arn" {
+    type =string
+}
+variable "vpc_id" {
+    type = string 
+}
+variable "permissions_boundary" {
+  type = string
+}
